e3e634aa7aa1c2210aaaa58c6cca2cce64a295a9
sam <sampurna.chapagainnn@gmail.com>
2023-01-07 17:15:06 +0000

    likes migration and schema files

---
+++ db/migrate/20230106183352_create_likes.rb
@@ -0,0 +1,11 @@
+class CreateLikes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :likes do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :topic, null: false, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :likes, [:user_id, :topic_id], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_03_182154) do
+ActiveRecord::Schema.define(version: 2023_01_06_183352) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 2022_12_03_182154) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["topic_id"], name: "index_comments_on_topic_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "topic_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["topic_id"], name: "index_likes_on_topic_id"
+    t.index ["user_id", "topic_id"], name: "index_likes_on_user_id_and_topic_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "topics", force: :cascade do |t|
@@ -45,5 +55,7 @@ ActiveRecord::Schema.define(version: 2022_12_03_182154) do
 
   add_foreign_key "comments", "topics"
   add_foreign_key "comments", "users"
+  add_foreign_key "likes", "topics"
+  add_foreign_key "likes", "users"
   add_foreign_key "topics", "users"
 end
