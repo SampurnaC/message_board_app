d68750ebfe2a03b1282a3a89c40cb613a96c2dad
sam <sampurna.chapagainnn@gmail.com>
2023-01-16 18:18:46 +0000

    add relationship between parent and child comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :topic
+
+  belongs_to :parent, class_name: "Comment", optional: true
+
+  # child comment
+  has_many :comments, foreign_key: :parent_id
 end
