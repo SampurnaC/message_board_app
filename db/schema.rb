79e30f1144388afdeee84ae2a64e875ee6c34176
sam <sampurna.chapagainnn@gmail.com>
2023-01-16 18:19:42 +0000

    update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_06_183352) do
+ActiveRecord::Schema.define(version: 2023_01_15_215435) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2023_01_06_183352) do
     t.integer "topic_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "parent_id"
     t.index ["topic_id"], name: "index_comments_on_topic_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
